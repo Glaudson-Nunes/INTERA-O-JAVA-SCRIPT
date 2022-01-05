ca1a96d5546b89b9b44fb56e22d362728f7a8da7
GLAUDSON NUNES <93484378+Glaudson-Nunes@users.noreply.github.com>
2022-01-04 21:40:39 -0300

    Add files via upload

---
+++ inde.js
@@ -0,0 +1,125 @@
+
+
+
+
+
+
+function mudar(input){
+
+    let h1 = document.getElementsByTagName("h2")[0]
+    h1.innerText = input.value;
+
+}
+
+function MF(){
+
+    let h2 = document.getElementsByTagName("h2")[0]
+    h2.style.fontSize = "30px"
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function TF1(){
+
+    let h2 = document.getElementsByTagName("h2")[0]
+    h2.style.fontSize = "60px"
+
+}
+
+function TF2(){
+
+    let h2 = document.getElementsByTagName("h2")[0]
+    h2.style.fontSize = "80px"
+
+}
+
+function TF3(){
+
+    let h2 = document.getElementsByTagName("h2")[0]
+    h2.style.fontSize = "100px"
+
+}
+
+
+
+
+
+
+
+
+
+
+
+function CT1(){
+
+    let h2 = document.getElementsByTagName("h2")[0]
+    h2.style.color = "blue"
+    
+
+}
+
+function CT2(){
+
+    let h2 = document.getElementsByTagName("h2")[0]
+    h2.style.color = "red"
+    
+
+}
+
+function CT3(){
+
+    let h2 = document.getElementsByTagName("h2")[0]
+    h2.style.color = "green"
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function ocultar(){
+
+    let h2 = document.getElementsByTagName("h2")[0]
+    h2.style.display = "none"
+
+
+}
+
+function aparecer(){
+
+    let h2 = document.getElementsByTagName("h2")[0]
+    h2.style.display = "block"
+
+}
+
+function Maisculo(){
+
+    let h2 = document.getElementsByTagName("h2")[0]
+    h2.style.textTransform = " uppercase"
+
+}
+
+function Minusculo(){
+
+    let h2 = document.getElementsByTagName("h2")[0]
+    h2.style.textTransform = "lowercase"
+
+}
